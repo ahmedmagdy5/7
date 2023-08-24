ad69a94e475ad0c2fb87c5cd21b5130fa781affb
ahmedmagdy5 <143043213+ahmedmagdy5@users.noreply.github.com>
2023-08-25 01:26:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Navbar
+var btn1 = document.querySelector(".nav-right");
+var btn2 = document.querySelector(".nav-right ul");
+
+btn1.onclick = function() {
+    btn2.classList.toggle("rs-d-block"); 
+}
+
+
+
